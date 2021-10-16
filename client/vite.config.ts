96fca95d0839e259d733ad244abee21e2da2f41c
Zenthae <zenthae@gmail.com>
2021-10-16 18:06:13 +0200

    add aliases

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src'),
+      },
+      {
+        find: '~',
+        replacement: resolve(__dirname, './src/components'),
+      },
+    ],
+  },
   plugins: [vue()],
 });
